772ec0b79d2db5775908dd66d4fe7f751e03a8bf
Prashanth <chinnuprashanth868@gmail.com>
2025-12-03 14:39:24 +0530

    added some poits

--- DAY_11/4_raw_model.py
+++ DAY_11/4_raw_model.py
@@ -1,18 +1,9 @@
 
 import os
 from langchain_openai import ChatOpenAI
-
-os.environ["OPENAI_API_KEY"] = "sk-..."
+from dotenv import load_dotenv
+load_dotenv()
 
 model = ChatOpenAI(model="gpt-4.1") 
-#         or
-# model = init_chat_model(
-#     "claude-sonnet-4-5-20250929",
-#     # Kwargs passed to the model:
-#     temperature=0.7,
-#     timeout=30,
-#     max_tokens=1000,
-# )
-        
 response = model.invoke("Why do parrots talk?")
 print(response.content)
